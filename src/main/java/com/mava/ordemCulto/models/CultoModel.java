4249f5b564cb342d7f85b8707e5b462ec215a0e2
Kauan Souza Lima <kauan.lima@sipam.gov.br>
2024-10-03 10:16:19 -0300

    Adfeat: adicionando as validações na minha classe CultoModel

--- src/main/java/com/mava/ordemCulto/models/CultoModel.java
+++ src/main/java/com/mava/ordemCulto/models/CultoModel.java
@@ -2,6 +2,9 @@ package com.mava.ordemCulto.models;
 
 import com.mava.ordemCulto.models.enums.TipoCulto;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,12 +21,17 @@ public class CultoModel {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cultos_seq")
     @SequenceGenerator(name = "cultos_seq", sequenceName = "cultos_seq", allocationSize = 1)
     private Integer id;
+    @NotNull
+    @Size(min = 8, max = 100)
     private String tituloCulto;
     //ENUM
     @Enumerated(EnumType.STRING)
     private TipoCulto tipoCulto;
+    @NotNull
     private LocalDate dataCulto;
+    @NotNull
     private String dirigente;
+    @NotNull
     private String horaProsperar;
     //Relacionamento com a classe OportunidadesModel
     @OneToMany(mappedBy = "culto", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
