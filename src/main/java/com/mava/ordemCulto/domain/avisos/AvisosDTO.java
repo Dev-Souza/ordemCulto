0649012219ce77456d4734b940912f62d539e3db
Dev-Souza <kauan.souza.dev@gmail.com>
2024-10-10 11:00:57 -0300

    feat: Criando os DTOs de culto e aviso

---
+++ src/main/java/com/mava/ordemCulto/domain/avisos/AvisosDTO.java
@@ -0,0 +1,15 @@
+package com.mava.ordemCulto.domain.avisos;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public record AvisosDTO(
+        Integer id,
+        String nomeAviso,
+        String referente,
+        LocalTime horarioEvento,
+        List<LocalDate>diasEvento,
+        Integer cultoId
+) {
+}
---
+++ src/main/java/com/mava/ordemCulto/domain/cultos/CultoDTO.java
@@ -0,0 +1,22 @@
+package com.mava.ordemCulto.domain.cultos;
+
+import com.mava.ordemCulto.domain.avisos.Avisos;
+import com.mava.ordemCulto.domain.equipe_intercessao.EquipeIntercessao;
+import com.mava.ordemCulto.domain.oportunidades.Oportunidades;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record CultoDTO(
+        Integer id,
+        String tituloCulto,
+        TipoCulto tipoCulto,
+        LocalDate dataCulto,
+        String dirigente,
+        String horaProsperar,
+        List<Oportunidades> oportunidades,
+        List<EquipeIntercessao> equipeIntercessao,
+        List<Avisos> avisos
+) {}
