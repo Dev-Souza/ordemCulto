f8b28f68752d917bc1ca3d5d9a5a16a5c5fe65f4
Dev-Souza <kauan.souza.dev@gmail.com>
2024-12-23 11:27:52 -0300

    feat: metodo addAviso feito

--- src/main/java/com/mava/ordemCulto/services/AvisosService.java
+++ src/main/java/com/mava/ordemCulto/services/AvisosService.java
@@ -1,11 +1,42 @@
 package com.mava.ordemCulto.services;
 
+import com.mava.ordemCulto.domain.avisos.Avisos;
+import com.mava.ordemCulto.domain.avisos.AvisosDTO;
+import com.mava.ordemCulto.domain.cultos.Culto;
 import com.mava.ordemCulto.repositories.AvisosRepository;
+import com.mava.ordemCulto.repositories.CultoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AvisosService {
     private final AvisosRepository avisosRepository;
+    private final CultoRepository cultoRepository;
+
+    private Avisos paraEntidade(AvisosDTO avisosDTO, Integer idCulto){
+        Avisos avisos = new Avisos();
+        avisos.setNomeAviso(avisosDTO.nomeAviso());
+        avisos.setReferente(avisosDTO.referente());
+        avisos.setHorarioEvento(avisosDTO.horarioEvento());
+        avisos.setDiasEvento(avisosDTO.diasEvento());
+        avisos.setCultoId(idCulto);
+        return avisos;
+    }
+
+    //ADD Aviso in Culto
+    public ResponseEntity<Culto> addAviso(Integer idCulto, AvisosDTO newAviso) {
+        //Buscando culto existente
+        Culto cultoBuscado = cultoRepository.findById(idCulto).orElseThrow(() -> new RuntimeException("Culto não encontrado"));
+        //Transformando o meu newAviso em entidade
+        Avisos novoAviso = paraEntidade(newAviso, idCulto);
+
+        //Adicionando esse aviso com os demais
+        cultoBuscado.getAvisos().add(novoAviso);
+        //Salvando este culto com o novo aviso
+        cultoRepository.save(cultoBuscado);
+
+        return ResponseEntity.ok(cultoBuscado);
+    }
 }
