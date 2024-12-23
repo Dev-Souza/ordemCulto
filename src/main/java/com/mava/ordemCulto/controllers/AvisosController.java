001c364d9b6a0b279207374e1edd7771437529e6
Dev-Souza <kauan.souza.dev@gmail.com>
2024-12-23 13:02:38 -0300

    feat: regra de negocio da classe avisos

--- src/main/java/com/mava/ordemCulto/controllers/AvisosController.java
+++ src/main/java/com/mava/ordemCulto/controllers/AvisosController.java
@@ -1,4 +1,35 @@
 package com.mava.ordemCulto.controllers;
 
+import com.mava.ordemCulto.domain.avisos.AvisosDTO;
+import com.mava.ordemCulto.domain.cultos.Culto;
+import com.mava.ordemCulto.services.AvisosService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aviso")
+@RequiredArgsConstructor
+@Validated
 public class AvisosController {
+    private final AvisosService avisosService;
+
+    //ADD Aviso In Culto
+    @PostMapping("/{idCulto}")
+    public ResponseEntity<Culto> addNewAviso(@PathVariable("idCulto") Integer idCulto, @RequestBody AvisosDTO newAviso) {return avisosService.addAviso(idCulto, newAviso);}
+
+    //GET ALL Avisos DE UM CULTO
+    @GetMapping("/{idCulto}")
+    public ResponseEntity<List<AvisosDTO>> getAllPorUmCulto(@PathVariable("idCulto") Integer idCulto) {return avisosService.getAllAvisosPorUmCulto(idCulto);}
+
+    //UPDATE
+    @PutMapping("/{idAviso}")
+    public ResponseEntity<AvisosDTO> updateAviso(@PathVariable("idAviso") Integer idAviso, @RequestBody AvisosDTO avisoUpdated) {return avisosService.updateAviso(idAviso, avisoUpdated);}
+
+    //DELETE
+    @DeleteMapping("/idAviso")
+    public ResponseEntity<Void> deleteAviso(@PathVariable("idAviso") Integer idAviso){return avisosService.deleteAviso(idAviso);}
 }
--- src/main/java/com/mava/ordemCulto/services/AvisosService.java
+++ src/main/java/com/mava/ordemCulto/services/AvisosService.java
@@ -6,8 +6,12 @@ import com.mava.ordemCulto.domain.cultos.Culto;
 import com.mava.ordemCulto.repositories.AvisosRepository;
 import com.mava.ordemCulto.repositories.CultoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -15,7 +19,7 @@ public class AvisosService {
     private final AvisosRepository avisosRepository;
     private final CultoRepository cultoRepository;
 
-    private Avisos paraEntidade(AvisosDTO avisosDTO, Integer idCulto){
+    private Avisos paraEntidade(AvisosDTO avisosDTO, Integer idCulto) {
         Avisos avisos = new Avisos();
         avisos.setNomeAviso(avisosDTO.nomeAviso());
         avisos.setReferente(avisosDTO.referente());
@@ -23,6 +27,17 @@ public class AvisosService {
         avisos.setDiasEvento(avisosDTO.diasEvento());
         avisos.setCultoId(idCulto);
         return avisos;
+    }
+
+    private AvisosDTO paraDTO(Avisos avisos) {
+        return new AvisosDTO(
+                avisos.getId(),
+                avisos.getNomeAviso(),
+                avisos.getReferente(),
+                avisos.getHorarioEvento(),
+                avisos.getDiasEvento(),
+                avisos.getCultoId()
+        );
     }
 
     //ADD Aviso in Culto
@@ -38,5 +53,54 @@ public class AvisosService {
         cultoRepository.save(cultoBuscado);
 
         return ResponseEntity.ok(cultoBuscado);
+    }
+
+    //GET ALL Avisos por um culto
+    public ResponseEntity<List<AvisosDTO>> getAllAvisosPorUmCulto(Integer idCulto) {
+        Culto cultoExistente = cultoRepository.getById(idCulto);
+        //Setar os avisos para fazer o return
+        //Converte os avisos para DTO
+        List<AvisosDTO> avisos = cultoExistente.getAvisos().stream()
+                .map(aviso -> new AvisosDTO(
+                        aviso.getId(),
+                        aviso.getNomeAviso(),
+                        aviso.getReferente(),
+                        aviso.getHorarioEvento(),
+                        aviso.getDiasEvento(),
+                        idCulto
+                ))
+                .collect(Collectors.toList());
+
+        //Retorna a lista de Avisos
+        return ResponseEntity.ok(avisos);
+    }
+
+    //GET BY ID AVISO
+    public ResponseEntity<AvisosDTO> getByIdAviso(Integer idAviso) {
+        return avisosRepository.findById(idAviso)
+                .map(avisos -> ResponseEntity.ok(paraDTO(avisos)))
+                .orElseGet(() -> ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .header("error", "Nenhum aviso encontrado!")
+                        .build());
+    }
+
+    //UPDATE AVISO
+    public ResponseEntity<AvisosDTO> updateAviso(Integer idAviso, AvisosDTO avisoUpdated) {
+        Avisos avisoBuscado = avisosRepository.findById(idAviso).orElseThrow(() -> new RuntimeException("Aviso não encontrado"));
+        avisoBuscado.setNomeAviso(avisoUpdated.nomeAviso());
+        avisoBuscado.setReferente(avisoUpdated.referente());
+        avisoBuscado.setHorarioEvento(avisoUpdated.horarioEvento());
+        avisoBuscado.setDiasEvento(avisoUpdated.diasEvento());
+        //Salvando este Aviso alterado
+        avisosRepository.save(avisoBuscado);
+        return ResponseEntity.ok(paraDTO(avisoBuscado));
+    }
+
+    //DELETE AVISO
+    public ResponseEntity<Void> deleteAviso (Integer idAviso){
+        ResponseEntity<AvisosDTO> avisoEncontrado = getByIdAviso(idAviso);
+        avisosRepository.deleteById(idAviso);
+        return ResponseEntity.noContent().build();
     }
 }
