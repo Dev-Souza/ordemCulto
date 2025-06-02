ebf8a95fb141c020421799b92a16bd5c390bf2b6
Kauan Souza Lima <kauan.lima@sipam.gov.br>
2025-06-02 13:18:16 -0300

    fix: name the cors

--- src/main/java/com/mava/ordemCulto/infra/security/CorsConfig.java
+++ src/main/java/com/mava/ordemCulto/infra/security/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Ou especifique os domínios permitidos
+                .allowedOrigins(
+                        "http://localhost:3000", // Para seu desenvolvimento local
+                        "http://localhost:5173", // Porta padrão do Vite, caso use
+                        "https://ordem-culto-front.vercel.app" // <<< SEU LINK CORRETO DA VERCEL
+                ) // Ou especifique os domínios permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Adicione "OPTIONS"
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite envio de credenciais (como cookies ou autenticação)
