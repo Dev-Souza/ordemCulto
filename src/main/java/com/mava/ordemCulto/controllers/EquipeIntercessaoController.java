f73a8b2ea1eada67da3716b7eaf98eb9fe84b15c
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-10 09:58:34 -0300

    fix: service de equipe

--- src/main/java/com/mava/ordemCulto/controllers/EquipeIntercessaoController.java
+++ src/main/java/com/mava/ordemCulto/controllers/EquipeIntercessaoController.java
@@ -1,6 +1,8 @@
 package com.mava.ordemCulto.controllers;
 
 import com.mava.ordemCulto.domain.cultos.CultoEntity;
+import com.mava.ordemCulto.domain.cultos.dto.CultoResponseDTO;
+import com.mava.ordemCulto.domain.equipe_intercessao.dto.EquipeIntercessaoRequestDTO;
 import com.mava.ordemCulto.domain.equipe_intercessao.dto.EquipeIntercessaoResponseDTO;
 import com.mava.ordemCulto.services.EquipeIntercessaoService;
 import lombok.RequiredArgsConstructor;
@@ -19,17 +21,17 @@ public class EquipeIntercessaoController {
 
     //ADD Intercessao In Culto
     @PostMapping("/{idCulto}")
-    public ResponseEntity<CultoEntity> addNewIntercessor(@PathVariable("idCulto") Long idCulto, @RequestBody EquipeIntercessaoResponseDTO newIntercessor) {return equipeIntercessaoService.addIntercesor(idCulto, newIntercessor);}
+    public ResponseEntity<CultoResponseDTO> addNewIntercessor(@PathVariable("idCulto") Long idCulto, @RequestBody EquipeIntercessaoRequestDTO newIntercessor) {return equipeIntercessaoService.addIntercesor(idCulto, newIntercessor);}
 
     //GET ALL Intercessores DE UM CULTO
     @GetMapping("/{idCulto}")
-    public ResponseEntity<List<EquipeIntercessaoResponseDTO>> getAllPorUmCulto(@PathVariable("idCulto") Integer idCulto) {return equipeIntercessaoService.getAllIntercessorPorUmCulto(idCulto);}
+    public ResponseEntity<List<EquipeIntercessaoResponseDTO>> getAllPorUmCulto(@PathVariable("idCulto") Long idCulto) {return equipeIntercessaoService.getAllIntercessorPorUmCulto(idCulto);}
 
     //UPDATE
     @PutMapping("/{idIntercessor}")
-    public ResponseEntity<EquipeIntercessaoResponseDTO> updateIntercessor(@PathVariable("idIntercessor") Integer idIntercessor, @RequestBody EquipeIntercessaoResponseDTO intercessorUpdated) {return equipeIntercessaoService.updateIntercessor(idIntercessor, intercessorUpdated);}
+    public ResponseEntity<EquipeIntercessaoResponseDTO> updateIntercessor(@PathVariable("idIntercessor") Long idIntercessor, @RequestBody EquipeIntercessaoResponseDTO intercessorUpdated) {return equipeIntercessaoService.updateIntercessor(idIntercessor, intercessorUpdated);}
 
     //DELETE
     @DeleteMapping("/idIntercessor")
-    public ResponseEntity<Void> deleteIntercessor(@PathVariable("idIntercessor") Integer idIntercessor) {return equipeIntercessaoService.deleteIntercessor(idIntercessor);}
+    public ResponseEntity<Void> deleteIntercessor(@PathVariable("idIntercessor") Long idIntercessor) {return equipeIntercessaoService.deleteIntercessor(idIntercessor);}
 }
--- src/main/java/com/mava/ordemCulto/services/EquipeIntercessaoService.java
+++ src/main/java/com/mava/ordemCulto/services/EquipeIntercessaoService.java
@@ -1,11 +1,15 @@
 package com.mava.ordemCulto.services;
 
 import com.mava.ordemCulto.domain.cultos.CultoEntity;
-import com.mava.ordemCulto.domain.equipe_intercessao.dto.EquipeIntercessaoResponseDTO;
+import com.mava.ordemCulto.domain.cultos.dto.CultoResponseDTO;
 import com.mava.ordemCulto.domain.equipe_intercessao.EquipeIntercessaoEntity;
+import com.mava.ordemCulto.domain.equipe_intercessao.dto.EquipeIntercessaoRequestDTO;
+import com.mava.ordemCulto.domain.equipe_intercessao.dto.EquipeIntercessaoResponseDTO;
+import com.mava.ordemCulto.infra.mapper.CultoMapper;
+import com.mava.ordemCulto.infra.mapper.EquipeIntercessaoMapper;
 import com.mava.ordemCulto.repositories.CultoRepository;
 import com.mava.ordemCulto.repositories.EquipeIntercessaoRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,50 +18,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EquipeIntercessaoService {
     private final EquipeIntercessaoRepository equipeIntercessaoRepository;
     private final CultoRepository cultoRepository;
-
-    private EquipeIntercessaoEntity paraEntidade(EquipeIntercessaoResponseDTO equipeIntercessaoDTO, Integer idCulto) {
-        EquipeIntercessaoEntity equipeIntercessao = new EquipeIntercessaoEntity();
-        equipeIntercessao.setNomeObreiro(equipeIntercessaoDTO.nomeObreiro());
-        equipeIntercessao.setCargo(equipeIntercessaoDTO.cargoEquipeIntercessao());
-        equipeIntercessao.setCultoId(idCulto);
-        return equipeIntercessao;
-    }
-
-    private EquipeIntercessaoResponseDTO paraDTO(EquipeIntercessaoEntity equipeIntercessao) {
-        return new EquipeIntercessaoResponseDTO(
-                equipeIntercessao.getId(),
-                equipeIntercessao.getNomeObreiro(),
-                equipeIntercessao.getCargo(),
-                equipeIntercessao.getCulto().getId()
-        );
-    }
+    private final EquipeIntercessaoMapper equipeIntercessaoMapper;
+    private final CultoMapper cultoMapper;
 
     //ADD Intercessor In Culto
-    public ResponseEntity<CultoEntity> addIntercesor(Long idCulto, EquipeIntercessaoResponseDTO newIntercessor) {
+    public ResponseEntity<CultoResponseDTO> addIntercesor(Long idCulto, EquipeIntercessaoRequestDTO newIntercessor) {
         //Buscando culto existente
         CultoEntity cultoBuscado = cultoRepository.findById(idCulto).orElseThrow(() -> new RuntimeException("Culto não encontrado"));
         //Tranformando o meu newIntercessor em entidade
-        EquipeIntercessaoEntity equipeIntercessao = paraEntidade(newIntercessor, idCulto);
+        EquipeIntercessaoEntity equipeIntercessao = equipeIntercessaoMapper.toEntity(newIntercessor);
 
         //Adicionando esse intercessor com os demais
         cultoBuscado.getEquipeIntercessao().add(equipeIntercessao);
         //Salvando este culto com o novo aviso
-        cultoRepository.save(cultoBuscado);
+        CultoEntity cultoSaved = cultoRepository.save(cultoBuscado);
 
-        return ResponseEntity.ok(cultoBuscado);
+        return ResponseEntity.ok(cultoMapper.toDTO(cultoSaved));
     }
 
     //GET ALL EquipeIntercessão por um culto
-    public ResponseEntity<List<EquipeIntercessaoResponseDTO>> getAllIntercessorPorUmCulto(Integer idCulto) {
+    public ResponseEntity<List<EquipeIntercessaoResponseDTO>> getAllIntercessorPorUmCulto(Long idCulto) {
         CultoEntity cultoExistente = cultoRepository.getById(idCulto);
         //Setar os intercessores para fazer o return
         //Converte os avisos ára DTO
         List<EquipeIntercessaoResponseDTO> intercessores = cultoExistente.getEquipeIntercessao().stream()
-                .map(intercessor -> paraDTO(intercessor))
+                .map(equipeIntercessaoMapper::toDTO)
                 .collect(Collectors.toList());
 
         //Retorna a lista de intercessores
@@ -65,9 +54,9 @@ public class EquipeIntercessaoService {
     }
 
     //GET BY ID INTERCESSOR
-    public ResponseEntity<EquipeIntercessaoResponseDTO> getByIdIntercessor(Integer idIntercessor) {
+    public ResponseEntity<EquipeIntercessaoResponseDTO> getByIdIntercessor(Long idIntercessor) {
         return equipeIntercessaoRepository.findById(idIntercessor)
-                .map(intercessores -> ResponseEntity.ok(paraDTO(intercessores)))
+                .map(intercessores -> ResponseEntity.ok(equipeIntercessaoMapper.toDTO(intercessores)))
                 .orElseGet(() -> ResponseEntity
                         .status(HttpStatus.NOT_FOUND)
                         .header("error", "Nenhum intercessor encontrado!")
@@ -75,17 +64,17 @@ public class EquipeIntercessaoService {
     }
 
     //UPDATE INTERCESSOR
-    public ResponseEntity<EquipeIntercessaoResponseDTO> updateIntercessor(Integer idIntercessor, EquipeIntercessaoResponseDTO intercessorUpdated) {
+    public ResponseEntity<EquipeIntercessaoResponseDTO> updateIntercessor(Long idIntercessor, EquipeIntercessaoResponseDTO intercessorUpdated) {
         EquipeIntercessaoEntity equipeIntercessao = equipeIntercessaoRepository.findById(idIntercessor).orElseThrow(() -> new RuntimeException("Aviso não encontrado"));
         equipeIntercessao.setNomeObreiro(intercessorUpdated.nomeObreiro());
         equipeIntercessao.setCargo(intercessorUpdated.cargoEquipeIntercessao());
         //Salvando este Intercessor alterado
         equipeIntercessaoRepository.save(equipeIntercessao);
-        return ResponseEntity.ok(paraDTO(equipeIntercessao));
+        return ResponseEntity.ok(equipeIntercessaoMapper.toDTO(equipeIntercessao));
     }
 
     //DELETE INTERCESSOR
-    public ResponseEntity<Void> deleteIntercessor(Integer idIntercessor) {
+    public ResponseEntity<Void> deleteIntercessor(Long idIntercessor) {
         ResponseEntity<EquipeIntercessaoResponseDTO> equipeIntercessaoEncontrado = getByIdIntercessor(idIntercessor);
         equipeIntercessaoRepository.deleteById(idIntercessor);
         return ResponseEntity.noContent().build();
